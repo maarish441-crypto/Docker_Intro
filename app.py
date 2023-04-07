d00ee5ee03b61b7f5021d7453a7b5ffdbbe04549
maarish441-crypto <maarish441@gmail.com>
2023-04-08 03:04:09 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# flask app for hello world
+
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET'])
+def home():
+    return "Hello World"
+
+
+
+if __name__=="__main__":
+    app.run(debug = True)
